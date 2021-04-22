f672b623e5d2fdee344a1f43c8a494adf6859a3d
zhanghang <153437803@qq.com>
2021-04-22 18:22:25 +0800

    fix code

--- lib_webviewplus/src/main/java/lib/kalu/webviewplus/WebViewPlus.java
+++ lib_webviewplus/src/main/java/lib/kalu/webviewplus/WebViewPlus.java
@@ -2,10 +2,13 @@ package lib.kalu.webviewplus;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.webkit.WebView;
 
 import androidx.annotation.Keep;
+import androidx.annotation.NonNull;
 
 import lib.kalu.webviewplus.core.WebViewCore;
+import lib.kalu.webviewplus.listener.OnWebStatusChangeListener;
 
 /**
  * description:
@@ -32,5 +35,31 @@ public final class WebViewPlus extends WebViewCore {
 
     public WebViewPlus(Context context, AttributeSet attrs, int defStyleAttr, boolean privateBrowsing) {
         super(context, attrs, defStyleAttr, privateBrowsing);
+    }
+
+    @Override
+    public void onPageFinished(@NonNull WebView view, @NonNull String url) {
+        super.onPageFinished(view, url);
+
+        if (null != onWebStatusChangeListener) {
+            onWebStatusChangeListener.onPageFinished(view, url);
+        }
+    }
+
+    @Override
+    public void onPageStarted(@NonNull WebView view, @NonNull String url) {
+        super.onPageStarted(view, url);
+
+        if (null != onWebStatusChangeListener) {
+            onWebStatusChangeListener.onPageStarted(view, url);
+        }
+    }
+
+    /****************/
+
+    private OnWebStatusChangeListener onWebStatusChangeListener;
+
+    public void setOnWebStatusChangeListener(@NonNull OnWebStatusChangeListener listener) {
+        this.onWebStatusChangeListener = listener;
     }
 }
---
+++ lib_webviewplus/src/main/java/lib/kalu/webviewplus/listener/OnWebStatusChangeListener.java
@@ -0,0 +1,18 @@
+package lib.kalu.webviewplus.listener;
+
+import android.webkit.WebView;
+
+import androidx.annotation.Keep;
+import androidx.annotation.NonNull;
+
+/**
+ * description:
+ * created by kalu on 2021-04-22
+ */
+@Keep
+public interface OnWebStatusChangeListener {
+
+    void onPageStarted(@NonNull WebView view, @NonNull String url);
+
+    void onPageFinished(@NonNull WebView view, @NonNull String url);
+}
